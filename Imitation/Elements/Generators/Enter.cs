75116a4193a4489ad3516ccece1b43a3b2b2b537
slawiko <shchaurouski.slava@gmail.com>
2017-03-05 20:01:37 +0300

    3: rebuilts generators

--- Imitation/Elements/Generators/Enter.cs
+++ Imitation/Elements/Generators/Enter.cs
@@ -1,49 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Imitation.Elements
 {
 	public sealed class Enter : Generator
 	{
-		private int _num;
-		private double _delay;
-		private Random _random;
-		//TODO: each element will have Distribution law
-		//private bool Distribution;
-
-		public Enter(int num)
+		Enter(int delay)
 		{
-			Transacts = new Queue<Transact>();
-			_num = num;
-			_delay = -1;
-			_random = new Random();
+			this.Next = delay;
 		}
 
-		public Enter(double delay)
+		public override int Next
 		{
-			Transacts = new Queue<Transact>();
-			_num = -1;
-			_delay = delay;
-			_random = new Random();
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			protected set
+			{
+				throw new NotImplementedException();
+			}
 		}
 
-		public Enter(int num, double delay)
+		protected override Transact Transact
 		{
-			Transacts = new Queue<Transact>();
-			_num = num;
-			_delay = delay;
-			_random = new Random();
+			get
+			{
+				throw new NotImplementedException();
+			}
+
+			set
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override Transact Exit()
+		{
+			throw new NotImplementedException();
 		}
 
 		public override void Process()
 		{
-			for (var i = 0; i < _num; i++)
-			{
-				var transact = new Transact(_random);
-				transact.History += "It's a " + i + " created transact. ";
-				Transacts.Enqueue(transact);
-				Give();
-			}
+			throw new NotImplementedException();
 		}
 	}
 }
--- Imitation/Elements/Generators/Generator.cs
+++ Imitation/Elements/Generators/Generator.cs
@@ -2,13 +2,6 @@
 {
 	public abstract class Generator : Element, IGiver
 	{
-		public event GiveDelegate ReadyToGive;
-		public virtual void Give()
-		{
-			if (ReadyToGive != null)
-			{
-				ReadyToGive(Transacts.Dequeue());
-			}
-		}
+		public abstract Transact Exit();
 	}
 }
