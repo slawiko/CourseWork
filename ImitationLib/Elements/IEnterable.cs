372387e87862517941b86a5388f3460203a3d65f
slawiko <shchaurouski.slava@gmail.com>
2017-05-09 20:02:53 +0300

    12: adds xmldocs

--- ImitationLib/Elements/IEnterable.cs
+++ ImitationLib/Elements/IEnterable.cs
@@ -2,6 +2,9 @@
 
 namespace ImitationLib.Elements
 {
+	/// <summary>
+	/// <see cref="IEnterable"/> interface is aplicable for those <see cref="Element"/> in which <see cref="Transact"/> can <see cref="Enter"/>
+	/// </summary>
 	public interface IEnterable
 	{
 		void Enter(Transact transact);
--- ImitationLib/Elements/IExitable.cs
+++ ImitationLib/Elements/IExitable.cs
@@ -2,6 +2,9 @@
 
 namespace ImitationLib.Elements
 {
+	/// <summary>
+	/// <see cref="IExitable"/> interface is aplicable for those <see cref="Element"/> from which <see cref="Transact"/> can <see cref="Exit"/>
+	/// </summary>
 	public interface IExitable
 	{
 		Transact Exit();
